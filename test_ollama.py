2309fdcb382bdec5628adbf9e8c3bbc82d00e902
jwsorima <jwsorima@gmail.com>
2025-02-07 14:55:54 +0800

    updated prompt for testing different model

--- test_ollama.py
+++ test_ollama.py
@@ -9,30 +9,40 @@ headers = {"Content-Type": "application/json"}
 
 def insert_vsdx_data(parsed_details: str) -> Dict[str, Any]:
   return {
-    "model": "llama3.2",
+    # "model": "llama3.2",
+    "model": "deepseek-r1:14b",
     "prompt": f"""
       Forget all details that I sent you before this prompt
 
       {parsed_details}
 
       These are data extracted from a VSDX file. Can you understand what the diagram represents?
-
-      I need two outputs:
-
-      Step-by-Step Process in sentence:
-        Provide a step-by-step process listed by numbers.
-        If a condition causes a loop back, state: Loops back to "[That part]", e.g., (Loops back to "[Development & User Testing]").
-        DO NOT include IDs or extra explanations.
-        Enclose all text inside shapes within [brackets].
-
-      And
-
-      Python Implementation:
-        Provide a Python implementation that follows this process exactly.
-        Use while loops for repetition and if conditions for checks.
-        The Python code must be logically correct.
-        DO NOT include comments or formatting syntax; return only plain Python code.
+      Can you understand the flow of diagram based on their positions and not rely on connectors reference?
+      Also give me a step by step process listed by numbers.
     """,
+    # "prompt": f"""
+    #   Forget all details that I sent you before this prompt
+
+    #   {parsed_details}
+
+    #   These are data extracted from a VSDX file. Can you understand what the diagram represents?
+
+    #   I need two outputs:
+
+    #   Step-by-Step Process in sentence:
+    #     Provide a step-by-step process listed by numbers.
+    #     If a condition causes a loop back, state: Loops back to "[That part]", e.g., (Loops back to "[Development & User Testing]").
+    #     DO NOT include IDs or extra explanations.
+    #     Enclose all text inside shapes within [brackets].
+
+    #   And
+
+    #   Python Implementation:
+    #     Provide a Python implementation that follows this process exactly.
+    #     Use while loops for repetition and if conditions for checks.
+    #     The Python code must be logically correct.
+    #     DO NOT include comments or formatting syntax; return only plain Python code.
+    # """,
     "stream": True
   }
 
@@ -46,8 +56,9 @@ def prompt_ai_with_vsdx_data(file_path: str) -> HTTPResponse:
   return urllib.request.urlopen(req)
 
 def _main():
-  file_path = "docs/converttoMPHtoKPH.vsdx"
+  file_path = "docs/Drawing2.vsdx"
   parsed_details = extract_vsdx_details(file_path)
+  print(parsed_details)
   data = insert_vsdx_data(parsed_details)
   json_data = json.dumps(data).encode("utf-8")
 
